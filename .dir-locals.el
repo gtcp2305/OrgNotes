de28a240d6daf677cea1c9a24443ae07f17de0ad
gtcp2305 <gtcp2305@yeah.net>
2018-11-19 21:05:51 +0800

    first add

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c++-mode . ((company-c-headers-path-user . '("." "../*/include"))
+              (company-c-headers-path-system . '("/usr/include" "/usr/local/include/*")))))
