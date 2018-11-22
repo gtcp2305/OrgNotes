97427a164885f616cae0eb582bbdd2b02a982eb1
gtcp2305 <gtcp2305@yeah.net>
2018-11-22 16:47:48 +0800

    add function pointer

--- .dir-locals.el
+++
@@ -1,5 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((c++-mode . ((company-c-headers-path-user . '("." "../*/include"))
-              (company-c-headers-path-system . '("/usr/include" "/usr/local/include/*")))))
